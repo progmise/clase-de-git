8cf6ba134ec8cce07ac8a16fbd267e657ff84daf
Chaves, Leonel Abel <lchaves@fi.uba.ar>
2023-11-02 20:05:14 -0300

    Se suma menu de calculadora

--- archivo.py
+++ archivo.py
@@ -1,4 +1,15 @@
 print('Hola Mundo')
 
 def main() -> None:
-    pass
+    opcion = input("Ingrese una opción: ")
+
+    if opcion == '+':
+        sumar()
+    elif opcion == '-':
+        restar()
+    elif opcion == '*':
+        multiplicar()
+    elif opcion == '/':
+        dividir()
+    else:
+        print('Opción inválida')
