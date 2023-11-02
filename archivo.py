c0f003a5767976c18a401f57397926fcd650680d
NestorPala <npalavecino@fi.uba.ar>
2023-11-02 20:11:02 -0300

    agrego funcion restar

--- archivo.py
+++ archivo.py
@@ -3,6 +3,12 @@ print('Hola Mundo')
 def multiplicar(n1:int,n2:int) -> int:
     return n1 * n2
 
+def restar() -> None:
+    numero_1 = input("Ingrese un numero: ")
+    numero_2 = input("Ingrese otro numero: ")
+    resta = numero_1 - numero_2
+    print("El resultado de la resta es:", resta)
+
 def main() -> None:
     opcion = input("Ingrese una opción: ")
     numero1 = int(input("Ingrese el primer numero:"))
