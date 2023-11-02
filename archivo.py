8635c325495ee886cf715d3ffd24e7363fd53b74
nicor12 <nromano@fi.uba.ar>
2023-11-02 20:09:39 -0300

    multiplicar cuestionable

--- archivo.py
+++ archivo.py
@@ -1,14 +1,18 @@
 print('Hola Mundo')
 
+def multiplicar(n1:int,n2:int) -> int:
+    return n1 * n2
+
 def main() -> None:
     opcion = input("Ingrese una opción: ")
-
+    numero1 = int(input("Ingrese el primer numero:"))
+    numero2 = int(input("Ingrese el segundo numero:"))
     if opcion == '+':
         sumar()
     elif opcion == '-':
-        restar()
-    elif opcion == '*':
-        multiplicar()
+        restar(numero7,numero4)
+    elif opcion == '**':
+        multiplicar(numero1,numero2)
     elif opcion == '/':
         dividir()
     else:
